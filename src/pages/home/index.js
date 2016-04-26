52e93b3e7a0767d20510638e99b2b1e28c7f8969
Harrison Fischberg <hrfischberg@gmail.com>
2016-04-25 23:14:06 -0600

    http.jsonp attempt at cors

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,10 +8,17 @@ export default () => {
 }
 
 const homeController = (['$scope', '$http', '$interval', '$q', ($scope, $http, $interval, $q) => {
-  // $interval(function (){
-    console.log('Update');
-    $http.get('https://shapeshiftnode.herokuapp.com/api/v1/get-coins').then(function(response) {
+
+  function makeAPICall() {
+    $http.jsonp('https://shapeshiftnode.herokuapp.com/api/v1/get-coins').then(function(response) {
       $scope.exchanges = response.data;
     });
-  // }, 2000);
+  }
+
+  makeAPICall();
+
+  $interval(function() {
+    makeAPICall()
+  }, 5000);
+
 }]);
