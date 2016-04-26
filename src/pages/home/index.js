db78b3b82ad8d7814f9e4506056059f66a6674d5
Harrison Fischberg <hrfischberg@gmail.com>
2016-04-26 12:34:49 -0600

    ngcloak and watch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,16 +10,22 @@ export default () => {
 const homeController = (['$scope', '$http', '$interval', '$q',
   ($scope, $http, $interval, $q) => {
 
-  function makeAPICall() {
-    $http.get('https://shapeshiftnode.herokuapp.com/api/v1/get-coins').then(function(response) {
-      $scope.exchanges = response.data;
+    function makeAPICall() {
+      $http.get('https://shapeshiftnode.herokuapp.com/api/v1/get-coins').then(function(response) {
+        $scope.exchanges = response.data;
+      });
+    }
+
+    makeAPICall();
+
+    $interval(function() {
+      makeAPICall()
+    }, 5000);
+
+    $scope.$watch("exchanges", function(newValue, oldValue) {
+      if (oldValue && newValue !== oldValue) {
+        console.log('Change happened');
+      }
     });
   }
-
-  makeAPICall();
-
-  $interval(function() {
-    makeAPICall()
-  }, 5000);
-
-}]);
+]);
